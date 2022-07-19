f28b976f336f4322aee23d0fcd42fdc63689acc6
Alexis Medina <giovanin007@gmail.com>
2022-07-18 23:15:25 -0500

    paso2

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+            <footer>
+                <>
+                    <p>derechos reservados 2022</p>
+                </>
+            </footer>
+    );
+}
+
+export default Footer;
